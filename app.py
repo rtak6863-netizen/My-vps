ee21bfd64603c7dbaf47c151f1b7968cec339d9a
rtak6863-netizen <rtak6863@gmail.com>
2026-05-16 02:17:48 +0430

    run bot
    Updated the Telegram bot token for authentication.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from telegram.ext import Application, CommandHandler
 import requests
 
-TOKEN = "8870873326:AAHwOosZfhlY6HHxL2IBdfJrythNPKpWCDA"
+TOKEN = "8772201536:AAGsVb-yS7MPK2gpgN51gVgk_U7enkCodYw"
 
 async def start(update, context):
     await update.message.reply_text("ربات فارکس طلا فعال شد ✅")
